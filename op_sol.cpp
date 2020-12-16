4ff2d4c51fe6b12e7a33fcb9d567aeb7826da6ad
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-16 21:26:59 +0800

    Create op_sol.cpp

---
+++ op_sol.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+#define ar array
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
+
+void solve() {
+	int n, k;
+	cin >> n >> k;
+	string s;
+	for(int i=0; i<n; ++i)
+		s+=char('a'+i%3);
+	cout << s << "\n";
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int t;
+	cin >> t;
+	while(t--)
+		solve();
+}
